0227e970424c2d1931afd5e91aec5cce9236bec6
Sergey13111 <boiko.sergey93@gmail.com>
2019-12-08 20:15:14 +0200

    field 2

--- app.js
+++ app.js
@@ -1,9 +1,4 @@
 let string = 'some test string';
-
-value = string[0].toUpperCase() + string.slice(1, -1) + string[string.length - 1].toUpperCase();
-
-console.log(value);
-
-value = `${ string[0].toUpperCase() }${ string.slice(1, -1) }${ string[string.length - 1].toUpperCase() }`;
+value = string.indexOf('string');
 
 console.log(value);
